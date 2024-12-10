50e3a8165b28b585ae79efe3cca8bf407a74aef1
koilyhuji <quynh.pg.64cntt@ntu.edu.vn>
2024-12-10 03:50:34 -0500

    TH6 FUI

--- ThQuizApp/app/src/main/java/th/phangiaquynh/thquizapp/MainActivity.java
+++ ThQuizApp/app/src/main/java/th/phangiaquynh/thquizapp/MainActivity.java
@@ -1,7 +1,10 @@
 package th.phangiaquynh.thquizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,34 +19,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        Button button1 = findViewById(R.id.button2);
+
+        LinearLayout button1 = findViewById(R.id.layoutc);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, tienganh.class);
+                Intent intent = new Intent(MainActivity.this, CppFrag.class);
                 startActivity(intent);
             }
         });
-        Button button2 = findViewById(R.id.button3);
+        LinearLayout button2 = findViewById(R.id.layoutjava);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Toan.class);
+                Intent intent = new Intent(MainActivity.this, JavaFrag.class);
                 startActivity(intent);
             }
         });
 
-        Button button3 = findViewById(R.id.button4);
+        LinearLayout button3 = findViewById(R.id.layoutpython);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LapTrinh.class);
+                Intent intent = new Intent(MainActivity.this, PythonFrag.class);
                 startActivity(intent);
             }
         });
